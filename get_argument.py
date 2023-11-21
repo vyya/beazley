37d62f7eb8f9b0acb3be0a08dec4a90dabb85055
neuro <yamsh@yandex.ru>
2023-11-21 23:29:02 +0600

    getting an argument with sys.argv method, .format method, exit from program with SystemExit

---
+++ get_argument.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# get_argument.py
+
+import sys
+
+if len(sys.argv) != 3:
+    raise SystemExit('Usage conditions: get_argument.py percent amount')
+percent = float(sys.argv[1])
+amount = int(sys.argv[2])
+
+first_month = amount + percent/360 * amount
+print(f'\nFirst month accumulated amount:{first_month}')
+print('Amount we have got assuming we had {}$ amount and {} percentage rate'.format(amount, percent))
+
+
+        
