d7d15c84e70dcd1fefbd9d41c549c65ec175fac6
neuro <yamsh@yandex.ru>
2023-11-23 16:27:37 +0600

    print formatted table using .format method

--- mortgage.py
+++ mortgage.py
@@ -13,7 +13,7 @@ extra_payment = 1000
 extra_payment_start_month = 1
 extra_payment_end_month = 60
 month = 0
-
+print('{:>5s} {:>10s} {:>10s} {:>10s}'.format('month', 'interest', 'remaining', 'price'))
 while principal > 0:
     month +=1
     interest = principal*(rate/12)
@@ -21,7 +21,7 @@ while principal > 0:
     if month in range(extra_payment_start_month, extra_payment_end_month+1):
         principal = principal - extra_payment
     total_paid += payment
-    print(month, interest, total_paid - interest, )
+    print('{:>5d} {:>10.2f} {:>10.2f} {:>10.2f}'.format(month, interest, total_paid - interest, principal ))
     
     
     #import pdb; pdb.set_trace()
