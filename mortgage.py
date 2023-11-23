5a9f41d734bd62c456b689d1b9362e063f1ea68c
neuro <yamsh@yandex.ru>
2023-11-23 15:42:59 +0600

    print key values without format

--- mortgage.py
+++ mortgage.py
@@ -17,15 +17,11 @@ month = 0
 while principal > 0:
     month +=1
     interest = principal*(rate/12)
-    print(f'Interst:{interest}')
     principal = principal + interest - payment
-    print(f'Principal before extra:{principal}')
     if month in range(extra_payment_start_month, extra_payment_end_month+1):
         principal = principal - extra_payment
-    print(f'Principal:{principal}')
     total_paid += payment
-    print(f'Total paid: {total_paid}')
-    print(f'Month:{month}')
+    print(month, interest, total_paid - interest, )
     
     
     #import pdb; pdb.set_trace()
