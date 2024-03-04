bca3ffe289272c28e38c6e592f67d6feff9fdc2f
neuro <yamsh@yandex.ru>
2024-03-04 16:26:34 +0500

    format and output in the text file added

--- mortgage1.py
+++ mortgage1.py
@@ -8,6 +8,8 @@ month = 0
 extra_payment = 1000
 extra_payment_start_month = 1
 extra_payment_end_month = 60
+out = open('schedule.txt', 'w') # open a file for writing
+print('{:>5s} {:>10s}{:>10s}{:>10s}'.format('Month', 'Interest', 'Remaining', 'Principal'), file = out)
 while principal > 0:
     month +=1
     if month <= extra_payment_end_month:
@@ -16,9 +18,12 @@ while principal > 0:
         total_payment = payment
 
     interest = principal*(rate/12)
-    print('Interest:', interest)
+    #print('Interest:', interest)
     #raise SystemExit(0)
     principal = principal + interest - total_payment
     #print('Principal:', principal)
     total_paid += total_payment
-print('Total paid:', total_paid, 'For period:', month/12)
+    print('{:>5d} {:>10.2f} {:>10.2f} {:>10.2f}'.format(month, interest,\
+    total_payment - interest, principal), file = out)
+print(('Total paid:', total_paid, 'For period:', month/12), file = out)
+out.close()
