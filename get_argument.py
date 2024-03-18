a7eca8fe1729e0fea45861d72188c8beb6a06835
neuro <yamsh@yandex.ru>
2024-03-18 15:58:46 +0500

    debugging code commented out

--- get_argument.py
+++ get_argument.py
@@ -8,9 +8,9 @@ if len(sys.argv) != 3:
 percent = float(sys.argv[1])
 amount = int(sys.argv[2])
 
-first_month = amount + percent/360 * amount
-import pdb; pdb.set_trace()
-print(f'\nFirst month accumulated amount:{first_month.text}')
+first_month = amount + percent/12 * amount
+#import pdb; pdb.set_trace()
+print(f'\nFirst month accumulated amount:{first_month}')
 print('Amount we have got assuming we had {}$ amount and {} percentage rate'.format(amount, percent))
 
 
