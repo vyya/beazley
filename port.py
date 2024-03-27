f2abb6a659ba5ef844492a497323ca9e0228921a
neuro <yamsh@yandex.ru>
2024-03-27 17:15:14 +0500

    script has been modified using csv library

--- port.py
+++ port.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
+import csv
 total_cost = 0.0
-with open('Data/portfolio.csv', 'r') as f:
+with open('Data/portfolio2.csv', 'r') as f:
+    rows = csv.reader(f)
     omit = next(f)
-    for line in f:
-        line = line.strip() #strip whitespaces
+    for row in rows:
+        print(row)
+        """ line = line.strip() #strip whitespaces
         line = line.split(',') 
-        """ split line by comma delimeter creates set of
-        lists """
+        split line by comma delimeter creates set of lists 
+    
         line[0] = line[0].strip('"')
         line[1] = line[1].strip('"') # get rid of quotation marks
-        line[2] = int(line[2])
-        line[3] = float(line[3]) #convert string to integer
-        total_cost += line[2] * line[3]
-        print(line)
+        """
+        row[2] = int(row[2])
+        row[3] = float(row[3]) #convert string to integer
+        total_cost+= row[2] * row[3]
+        print(row)
     print(f'Total cost of all shares is {total_cost}')
