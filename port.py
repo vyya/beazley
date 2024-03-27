c43a96adde9c2c7c91ddd92c55772dfb83e0e214
neuro <yamsh@yandex.ru>
2024-03-27 16:41:46 +0500

    total cost calculation amended

--- port.py
+++ port.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+total_cost = 0.0
 with open('Data/portfolio.csv', 'r') as f:
     omit = next(f)
     for line in f:
@@ -10,6 +11,6 @@ with open('Data/portfolio.csv', 'r') as f:
         line[1] = line[1].strip('"') # get rid of quotation marks
         line[2] = int(line[2])
         line[3] = float(line[3]) #convert string to integer
-        total_cost = line[2] * line[3]
+        total_cost += line[2] * line[3]
         print(line)
     print(f'Total cost of all shares is {total_cost}')
