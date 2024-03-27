64bcec3de09f80daf8fd8f350df39aa7de9ab7b4
neuro <yamsh@yandex.ru>
2024-03-27 16:14:28 +0500

    shibang created, data cleaned and converted, calculation performed

--- port.py
+++ port.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python3
 with open('Data/portfolio.csv', 'r') as f:
+    omit = next(f)
     for line in f:
         line = line.strip() #strip whitespaces
+        line = line.split(',') 
+        """ split line by comma delimeter creates set of
+        lists """
+        line[0] = line[0].strip('"')
+        line[1] = line[1].strip('"') # get rid of quotation marks
+        line[2] = int(line[2])
+        line[3] = float(line[3]) #convert string to integer
+        total_cost = line[2] * line[3]
         print(line)
+    print(f'Total cost of all shares is {total_cost}')
