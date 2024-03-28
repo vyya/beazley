50b23a77457700dc4b1f22f64c57ac9770da25c0
neuro <yamsh@yandex.ru>
2024-03-28 15:21:14 +0600

    code simplified using csv module

--- port.py
+++ port.py
@@ -1,13 +1,15 @@
+import csv
 total = 0.0
-with open('Data/portfolio.csv', 'r') as f:
-    headers = next(f)
-    for line in f:
-        line = line.strip() # strip whitespace off
+with open('Data/portfolio3.csv', 'r') as f:
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+        """ line = line.strip() # strip whitespace off
         parts = line.split(',')
         parts[0] = parts[0].strip('"')
-        parts[1] = parts[1].strip('"')
-        parts[2] = int(parts[2])
-        parts[3] = float(parts[3])
-        total += parts[2] * parts[3]
-        print(parts)
+        parts[1] = parts[1].strip('"') """
+        row[2] = int(row[2])
+        row[3] = float(row[3])
+        total += row[2] * row[3]
+        print(row)
     print('Total cost=', total)
