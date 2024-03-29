91c0e85ece2749db881fb01c3d9a15eba39028ce
neuro <yamsh@yandex.ru>
2024-03-29 18:52:10 +0600

    errors handling

--- port.py
+++ port.py
@@ -1,6 +1,8 @@
 import csv
 import glob
-def sev_files(filename):
+def sev_files(filename, *, errors = 'warn'):
+    if errors not in {'warn', 'silent', 'raise'}:
+        raise ValueError("Errors must be one of 'warn', 'silent', 'raise'")
     total = 0.0
     with open(filename, 'r') as f:
         rows = csv.reader(f)
@@ -14,16 +16,21 @@ def sev_files(filename):
             try:
                 row[2] = int(row[2])
                 row[3] = float(row[3])
-            except ValueError as err:
-                print('Row:', rownumber, 'Bad row', row)
-                print('Reason', err)
-                continue # skips to the next row  
-            total += row[2] * row[3]
-            print(row)
+            except Exception as err:
+                if errors == 'warn':
+                    print('Row:', rownumber, 'Bad row', row)
+                    print('Reason', err)
+                elif errors == 'raise':
+                    raise # Re-raises the last exception
+                else:
+                    pass
+            continue # skips to the next row  
+        total += row[2] * row[3]
+        
     return total
 """ files = glob.glob('Data/portfolio*.csv')
 for file in files:
     data = file
     total = sev_files(data) """
-total = sev_files('Data/missing.csv')
+total = sev_files('Data/missing.csv', errors = 'silent')
 print('Total cost=', total)
